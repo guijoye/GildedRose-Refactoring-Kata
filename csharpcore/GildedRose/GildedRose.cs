2486972a8c69f2264bb9093bc00c614e628c3125
Guijoye <guillaume@joye-mail.fr>
2022-10-18 12:24:48 +0200

    Refacto: Isolate backstage passes processing

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -6,6 +6,9 @@ namespace GildedRoseKata
     {
         IList<Item> Items;
         private const string AgedBrieItemName = "Aged Brie";
+        private const string BackstagePassesItemName = "Backstage passes to a TAFKAL80ETC concert";
+        private const string SulfurasItemName = "Sulfuras, Hand of Ragnaros";
+        private const string ConjuredItemName = "Conjured";
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -24,62 +27,63 @@ namespace GildedRoseKata
             return true;
         }
 
+        public bool ProcessBackstageItem(Item item)
+        {
+            if (item.Name != BackstagePassesItemName)
+                return false;
+
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+
+                if (item.SellIn < 11)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+
+                if (item.SellIn < 6)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+            }
+
+            if (item.SellIn <= 0)
+                item.Quality = item.Quality - item.Quality;
+
+            item.SellIn = item.SellIn - 1;
+
+            return true;
+        }
+
         public void UpdateQuality()
         {
             foreach (var item in Items)
             {
-                if (item.Name.Equals("Sulfuras, Hand of Ragnaros"))
+                if (item.Name.Equals(SulfurasItemName))
                     continue;
 
                 if (ProcessBrieItem(item))
                     continue;
 
-                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.Quality > 0)
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
+                if (ProcessBackstageItem(item))
+                    continue;
 
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-                        }
-                    }
+                if (item.Quality > 0)
+                {
+                    item.Quality = item.Quality - 1;
                 }
 
                 if (item.SellIn <= 0)
                 {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Quality > 0)
                     {
-                        if (item.Quality > 0)
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
+                        item.Quality = item.Quality - 1;
                     }
                 }
 
