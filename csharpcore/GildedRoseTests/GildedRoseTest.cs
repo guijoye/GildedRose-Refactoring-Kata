952ad6be261069e1cd7952e4bd8719af0d0b0008
Guijoye <guillaume@joye-mail.fr>
2022-10-18 11:07:13 +0200

    Unit test for specs 1: Once the sell by date has passed, Quality degrades twice as fast

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -15,5 +15,27 @@ namespace GildedRoseTests
             app.UpdateQuality();
             Assert.Equal(ExpectedNameAfterUpdate, Items[0].Name);
         }
+
+        [Theory]
+        [InlineData("TestItem", 10, 10)]
+        [InlineData("TestItem", 1, 10)]
+        public void UpdateQuality_QualityDegrade_IsOk(string ItemName, int SellIn, int Quality)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = ItemName, SellIn = SellIn, Quality = Quality } };
+            GildedRose app = new(Items);
+            app.UpdateQuality();
+            Assert.Equal(Quality - 1, Items[0].Quality);
+        }
+
+        [Theory]
+        [InlineData("TestItem", 0, 10)]
+        [InlineData("TestItem", -5, 10)]
+        public void UpdateQuality_QualityDegradeTwiceAsFastWhenSellPassed_IsOk(string ItemName, int SellIn, int Quality)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = ItemName, SellIn = SellIn, Quality = Quality } };
+            GildedRose app = new(Items);
+            app.UpdateQuality();
+            Assert.Equal(Quality  - 2, Items[0].Quality);
+        }
     }
 }
